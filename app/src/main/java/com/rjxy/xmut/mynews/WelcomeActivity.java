1d8f90fdf3fb507deca1410598270245eabd3165
BestLifeW <lovecanon0823@gmail.com>
2016-06-29 18:38:53 +0800

    Initial commit

---
+++ app/src/main/java/com/rjxy/xmut/mynews/WelcomeActivity.java
@@ -0,0 +1,55 @@
+package com.rjxy.xmut.mynews;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity {
+
+
+    boolean  isFirstIn=false;
+
+    private  static final  int GOHOME=1000;
+    private  static  final  int GOGUIDE=1001;
+
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            if(msg.what==GOHOME){
+                goHome();
+            }else {
+                goGuide();
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        SharedPreferences preferences = getSharedPreferences("hhj", MODE_PRIVATE);
+
+        isFirstIn=preferences.getBoolean("isFirstIn",true);
+        if(isFirstIn){
+            handler.sendEmptyMessageDelayed(GOGUIDE,2000);
+            SharedPreferences.Editor editor=preferences.edit();
+            editor.putBoolean("isFirstIn",false);
+            editor.commit();
+        }else {
+            handler.sendEmptyMessageDelayed(GOHOME,2000);
+        }
+    }
+
+    private  void goHome(){
+        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    private void goGuide(){
+        Intent intent = new Intent(getApplicationContext(),GuideAcitivty.class);
+        startActivity(intent);
+        finish();
+    }
+}
