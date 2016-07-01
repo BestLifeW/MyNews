1f865fde12ca6a1cf353999cfe934e769363ef79
BestLifeW <lovecanon0823@gmail.com>
2016-07-01 12:01:01 +0800

    增加了Fragment

--- app/src/main/java/com/rjxy/xmut/mynews/MainActivity.java
+++ app/src/main/java/com/rjxy/xmut/mynews/MainActivity.java
@@ -1,6 +1,11 @@
 package com.rjxy.xmut.mynews;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.MenuItemCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,13 +15,23 @@ import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Toast;
+
+import com.rjxy.xmut.mynews.Adatper.FragViewAdapter;
+import com.rjxy.xmut.mynews.fragment.Tab_Fragment_1;
+import com.rjxy.xmut.mynews.fragment.Tab_Fragment_2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+
+    private TabLayout tabLayout;
+    private List<Fragment> fragmentsList;//fragment容器
+    private List<String> titleList;//标签容器
     private DrawerLayout mDrawerLayout;
     private Toolbar mTooblar;
     private SearchView mSearchView;
@@ -26,7 +41,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initData();
+        initFragment();
     }
+
+    private void initFragment() {
+            tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+            viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+            fragmentsList = new ArrayList<>();
+            titleList = new ArrayList<>();
+
+            fragmentsList.add(new Tab_Fragment_1());
+            fragmentsList.add(new Tab_Fragment_2());
+           //将fragment添加到fragmentList的list容器里
+            for (int i = 0; i < 2; i++) { //重复添加 只是填充  没什么用
+                fragmentsList.add(new Tab_Fragment_1());
+            }
+            titleList.add("新闻");
+            titleList.add("体育");
+
+            tabLayout.setTabMode(TabLayout.MODE_FIXED);//tab的模式如果标签多的话用MODE_SCROLLABLE  少的话用MODE_FIXED
+            //tabLayout.setBackgroundColor(Color.BLUE);
+
+             FragViewAdapter adapter = new FragViewAdapter(getSupportFragmentManager(), fragmentsList, titleList);
+            viewPager.setAdapter(adapter);
+            tabLayout.setupWithViewPager(viewPager);
+            tabLayout.setTabsFromPagerAdapter(adapter);
+
+    }
+
     private  void initData(){
         //设置drawerLayout的事件
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerlayout);
@@ -68,4 +111,7 @@ public class MainActivity extends AppCompatActivity {
         mShareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(item2);
         return true;
     }
+
+
+
 }
---
+++ app/src/main/java/com/rjxy/xmut/mynews/fragment/Tab_Fragment_2.java
@@ -0,0 +1,29 @@
+package com.rjxy.xmut.mynews.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.rjxy.xmut.mynews.R;
+
+
+/**
+ *
+ */
+public class Tab_Fragment_2 extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewTab_2 = inflater.inflate(R.layout.fragment_content1, container, false);
+        return viewTab_2;
+    }
+
+
+
+}
