a6261a081dc945ba9d77bda8e0621cb7acde0409
845462240 <845462240@qq.com>
2016-07-06 18:58:18 +0800

    删除了那个测试

--- app/src/main/java/com/rjxy/xmut/mynews/activity/SettingActivity.java
+++ app/src/main/java/com/rjxy/xmut/mynews/activity/SettingActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 import com.makeramen.roundedimageview.RoundedImageView;
@@ -14,11 +15,12 @@ import com.rjxy.xmut.mynews.utilis.PrefUtils;
 public class SettingActivity extends AppCompatActivity {
 
     private Toolbar mTooblar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String theme = PrefUtils.getString(this, "theme", "");
         if (!"".equals(theme)) {
-            switch (theme){
+            switch (theme) {
                 case "1":
                     setTheme(R.style.AppTheme1);
                     break;
@@ -50,10 +52,56 @@ public class SettingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
         mTooblar = (Toolbar) findViewById(R.id.tl_custom);
+        setSupportActionBar(mTooblar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        mTooblar.setTitle("设置");
+
+        if (!"".equals(theme)) {
+            switch (theme) {
+                case "1":
+                    findViewById(R.id.imageView1).setVisibility(View.VISIBLE);
+                    break;
+                case "2":
+                    findViewById(R.id.imageView2).setVisibility(View.VISIBLE);
+                    break;
+                case "3":
+                    findViewById(R.id.imageView3).setVisibility(View.VISIBLE);
+                    break;
+                case "4":
+                    findViewById(R.id.imageView4).setVisibility(View.VISIBLE);
+                    break;
+                case "5":
+                    findViewById(R.id.imageView5).setVisibility(View.VISIBLE);
+                    break;
+                case "6":
+                    findViewById(R.id.imageView6).setVisibility(View.VISIBLE);
+                    break;
+                case "7":
+                    findViewById(R.id.imageView7).setVisibility(View.VISIBLE);
+                    break;
+                case "8":
+                    findViewById(R.id.imageView8).setVisibility(View.VISIBLE);
+                    break;
+                default:
+                    break;
+            }
+        }
 
     }
-    
-    public void onClick(View view){
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onClick(View view) {
         findViewById(R.id.imageView1).setVisibility(View.INVISIBLE);
         findViewById(R.id.imageView2).setVisibility(View.INVISIBLE);
         findViewById(R.id.imageView3).setVisibility(View.INVISIBLE);
@@ -62,60 +110,52 @@ public class SettingActivity extends AppCompatActivity {
         findViewById(R.id.imageView6).setVisibility(View.INVISIBLE);
         findViewById(R.id.imageView7).setVisibility(View.INVISIBLE);
         findViewById(R.id.imageView8).setVisibility(View.INVISIBLE);
-        String str="";
-        switch (view.getId()){
-            case R.id.color1 :
+        String str = "";
+        switch (view.getId()) {
+            case R.id.color1:
                 findViewById(R.id.imageView1).setVisibility(View.VISIBLE);
-                RoundedImageView roundedImageView=(RoundedImageView)view;
-                mTooblar.setBackgroundColor(Color.parseColor("#FF6347"));
-                str="1";
+                RoundedImageView roundedImageView = (RoundedImageView) view;
+                str = "1";
 
                 break;
-            case R.id.color2 :
+            case R.id.color2:
                 findViewById(R.id.imageView2).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#FFE4C4"));
-                PrefUtils.setString(this,"theme","#FF6347");
-                str="2";
+                str = "2";
                 break;
-            case R.id.color3 :
+            case R.id.color3:
                 findViewById(R.id.imageView3).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#DAA520"));
-                str="3";
+                str = "3";
                 break;
-            case R.id.color4 :
+            case R.id.color4:
                 findViewById(R.id.imageView4).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#EE82EE"));
-                str="4";
+                str = "4";
                 break;
-            case R.id.color5 :
+            case R.id.color5:
                 findViewById(R.id.imageView5).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#DCDCDC"));
-                str="5";
+                str = "5";
                 break;
-            case R.id.color6 :
+            case R.id.color6:
                 findViewById(R.id.imageView6).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#D2B48C"));
-                str="6";
+                str = "6";
                 break;
-            case R.id.color7 :
+            case R.id.color7:
                 findViewById(R.id.imageView7).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#9370DB"));
-                str="7";
+                str = "7";
                 break;
-            case R.id.color8 :
+            case R.id.color8:
                 findViewById(R.id.imageView8).setVisibility(View.VISIBLE);
-                mTooblar.setBackgroundColor(Color.parseColor("#3CB371"));
-                str="8";
+                str = "8";
                 break;
-            default: break;
+            default:
+                break;
         }
-        PrefUtils.setString(this,"theme",str);
-        Intent intent=new Intent();
-        intent.setClass(this,MainActivity.class);
+        PrefUtils.setString(this, "theme", str);
+        Intent intent = new Intent();
+        intent.setClass(this, MainActivity.class);
         startActivity(intent);
         this.finish();
 
     }
 
-    
+
 }
